c23a8e1ee50f39f123259b6606618be28b5245f4
Julien Valleton <julien.valleton@gmail.com>
2023-09-14 16:56:43 +0200

    Ajout du menu de navigation et du logo

--- src/components/Erreur.jsx
+++ src/components/Erreur.jsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom"
 
 export default function Erreur() {
   return (
-    <div>Erreur 404
+    <div>
+        <h1>Erreur 404</h1>
     <Link to="/">
-   <p>Retour à la page d'accueil</p> 
+        <p>Retour à la page d'accueil</p> 
     </Link>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import { Link, NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
+import Logo from "../logo-kasa.png"
 
 export default function Navbar() {
   return (
-    <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/about-us">A propos</Link>
+
+    <nav className="navbar">
+        <div>
+            <img src={Logo} alt="logo" className="logo"/>
+        </div>
+        <div>
+            <Link to="/" className="menu">Accueil</Link>
+            <Link to="/about-us" className="menu">A propos</Link>
+        </div>
     </nav>
   )
 }
