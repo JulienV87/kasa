97474eed887fbafd8eca9ba43474b9afe91ec462
Julien Valleton <julien.valleton@gmail.com>
2023-10-13 17:00:15 +0200

    Gestion des erreurs ID dans Accomodation.jsx

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -3,22 +3,24 @@ import { useParams } from "react-router-dom";
 import DATA from '../assets/data';
 import Carousel from '../components/Carousel'
 import DescriptionAccomodation from '../components/DescriptionAccomodation';
+import { Navigate } from "react-router-dom";
+
+
+
 
 
 export default function Accomodation() {
   const {id} = useParams();
   const targetApartment = DATA.find((apartment) => apartment.id === id);
-
+  if (!targetApartment) {
+    return (
+      <Navigate to="/erreur-404" replace={true} />
+    )
+  }
   return (
     <div>
-      
       <Carousel apartment={targetApartment}/>
-
-     
-          <DescriptionAccomodation
-            apartment={targetApartment}
-          />
-       
+      <DescriptionAccomodation apartment={targetApartment}/>
     </div>
   )
 }
